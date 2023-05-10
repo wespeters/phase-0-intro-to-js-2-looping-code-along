8f7e5a2ec3dc9757c4683ca3f68d49149430c1c7
Wesley Peters <wesleypeters@gmail.com>
2023-05-10 16:34:31 -0600

    added functions writeCards and countDown

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = [];
+const event = [];
+
+function writeCards(names, event){
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return cards
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
