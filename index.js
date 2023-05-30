c9178b808ad1ef5a55c2f7b6845fb180fbdef90b
Wesley Peters <wesleypeters@gmail.com>
2023-05-29 19:13:31 -0600

    add for loop and while loop

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ function writeCards(names, event){
         cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     };
     return cards
-}
+};
+// console.log(writeCards(["Charlie", "Duke", "Priscila", "Leonard"], "Father\'s Day"))
 
 function countDown(n) {
     while (n >= 0) {
         console.log(n--);
     }
-}
+};
+// countDown(10)
+
